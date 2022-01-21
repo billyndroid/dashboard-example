2c28e761fb4fc68ec00edf84aa891b6215157753
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-21 09:10:58 +0000

    update
    js

--- scripts/main.js
+++ scripts/main.js
@@ -17,4 +17,21 @@ closeBtn.addEventListener(`click`, () => {
 
 themeToggler.addEventListener(`click`, () => {
     document.body.classList.toggle(`dark-theme-variables`);
+
+    themeToggler.querySelector(`span`).classList.toggle(`active`);
 });
+
+//fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement(`tr`);
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === `Declined` ? `danger` : order.shipping === `pending` ? `warning`: `primary`}">${order.shipping}</td>
+                        <td class="primary">Details</td>
+                    `;
+
+                    tr.innerHTML = trContent;
+                    document.querySelector(`table tbody`).appendChild(tr);
+})
---
+++ scripts/orders.js
@@ -0,0 +1,39 @@
+const Orders = [
+    {
+        productName: `Foldable Mini Drone`,
+        productNumber: `85631`,
+        paymentStatus: `Due`,
+        shipping: `Pending`
+    },
+    {
+        productName: `LAVENDER KF102 Drone`,
+        productNumber: `36378`,
+        paymentStatus: `Refunded`,
+        shipping: `Declined`
+    },
+    {
+        productName: `Ruko F11 Pro Drone`,
+        productNumber: `49347`,
+        paymentStatus: `Due`,
+        shipping: `Pending`
+    },
+    {
+        productName: `Drone with Camera Drone`,
+        productNumber: `96936`,
+        paymentStatus: `Paid`,
+        shipping: `Delivered`
+    },
+    {
+        productName: `GPS 4k Drone`,
+        productNumber: `22821`,
+        paymentStatus: `Due`,
+        shipping: `Pending`
+    },
+    {
+        productName: `Foldable Mini Drone`,
+        productNumber: `85631`,
+        paymentStatus: `Due`,
+        shipping: `Pending`
+    },
+
+]
