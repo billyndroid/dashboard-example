41d8c0f0bc65d6e8815ffaafc56d95755c2ab679
billyndroid <70548591+billyndroid@users.noreply.github.com>
2023-12-12 13:15:17 +0000

    Update to content
    Added some content to flash out idea

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,38 +2,38 @@ const Orders = [
     {
         productName: `S&P`,
         productNumber: `£33.94`,
-        paymentStatus: `Due`,
+        paymentStatus: `31.70 - (+.22)`,
         shipping: `Pending`
     },
     {
         productName: `QQQ`,
         productNumber: `£33.94`,
-        paymentStatus: `Refunded`,
+        paymentStatus: `31.70 - (+.42)`,
         shipping: `Declined`
     },
     {
-        productName: `Ruko F11 Pro FTSE`,
+        productName: `FTSE`,
         productNumber: `£33.94`,
-        paymentStatus: `Due`,
-        shipping: `Pending`
+        paymentStatus: `£31.70 - (+.75)`,
+        shipping: `Active`
     },
     {
         productName: `Oil`,
         productNumber: `£33.94`,
-        paymentStatus: `Paid`,
-        shipping: `Delivered`
+        paymentStatus: `£31.70 - (-1.05)`,
+        shipping: `Active`
     },
     {
         productName: `Gold`,
         productNumber: `£33.94`,
-        paymentStatus: `Due`,
-        shipping: `Natural Gas`
+        paymentStatus: `£31.70 - (+1.75)`,
+        shipping: `Pending`
     },
     {
         productName: `Natural Gas`,
         productNumber: `£33.94`,
-        paymentStatus: `Due`,
-        shipping: `Pending`
+        paymentStatus: `£31.70 - (+.75)`,
+        shipping: `Active`
     },
 
 ]
